14d376df1cd4fe9d316d9d7fd80ffc34826ef5f7
Valerii Kostinskiy <valerii.kostynskyi@gmail.com>
2024-03-05 14:04:16 +0200

    rename / change structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:test_riverpod/view/home_screen.dart/search_screen_view.dart';
+import 'package:test_riverpod/view/home_screen.dart/home_screen.dart';
 
 void main() {
   runApp(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test_riverpod/view/home_screen.dart/search_screen_view.dart';
+import 'package:test_riverpod/view/home_screen.dart/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
